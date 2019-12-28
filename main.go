a75ed556c963a40f834e7dcb49b60ba2e63e7bb7
lifzhu <lifzhu@tencent.com>
2019-12-28 22:21:03 +0800

    typo

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 
 	// graceful shutdown servers
 	quit := make(chan os.Signal)
-	// kill (no param) default send syscanll.SIGTERM
+	// kill (no param) default send syscall.SIGTERM
 	// kill -2 is syscall.SIGINT
 	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
